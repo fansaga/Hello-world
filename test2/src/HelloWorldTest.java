74696321ffa2e569afdd083b9acca803ec38118b
Fansaga <kazuo92273@yahoo.co.jp>
2012-07-17 23:37:50 +0900

    still test sorry

---
+++ test2/src/HelloWorldTest.java
@@ -0,0 +1,15 @@
+
+
+public class HelloWorldTest
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world!");
+		System.out.println("test2");
+	}
+
+}
